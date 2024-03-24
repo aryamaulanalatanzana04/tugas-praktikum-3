1eeefe877dea3b4d2086b9de2d7c174279ee4c28
Arya Maulana <kowekm42@gmail.com>
2024-03-24 08:55:48 +0700

    first commit

--- resources/view/welcome.blade.php
+++ resources/view/welcome.blade.php
@@ -1,3 +1,26 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    @vite('resources/sass/app.scss')
+</head>
+<body>
+    <div class="container m-5">
+
+        <img class="img-thumbnail" src="{{ Vite::asset('resources/images/main.svg') }}" alt="image">
+    </div>
+    @vite('resources/js/app.js')
+</body>
+</html> --}}
+
+
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -295,10 +318,46 @@ me-2"></i>Auto</small></button></li>
         </div>
     </div>
 
-
 </body>
 
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 {{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
